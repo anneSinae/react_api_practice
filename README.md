e40d2aed591e10db0ab78fe86c4208cc17b13eb9
anne <aoicielv@gmail.com>
2020-11-01 20:32:29 +0900

    Manage api request by custom hook : show Individual data when list click

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 3. Manage api request by useReducer : outside file(asyncReducer.js)
 4. Manage api request by custom hook : useAsync.js
 5. Manage api request by custom hook : rendering data by button click 
+6. Manage api request by custom hook : show Individual data when list click
--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import useAsync from './useAsync';
+import User from './User';
 
 async function getUsers() {
   const response = await axios.get('http://jsonplaceholder.typicode.com/users');
@@ -9,6 +10,7 @@ async function getUsers() {
 
 function Users() {
   const [state, refetch] =useAsync(getUsers, [], true);
+  const [userId, setUserId] = useState(null);
 
   const { loading, data : users, error } = state;
   if (loading) return <div>loading..</div>
@@ -19,12 +21,13 @@ function Users() {
     <>
       <ul>
         {users.map(user => (
-        <li key={user.id}>
+        <li key={user.id} onClick={() => setUserId(user.id)}>
           {user.username} ({user.name})
         </li>
         ))}
       </ul>
       <button onClick={refetch}>request again</button>
+      { userId && <User id={userId} />}
     </>
   );
 }
